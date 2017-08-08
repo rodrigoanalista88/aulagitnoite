c5e37f982e43c9b7c88295bec8a8318d9db7b486
Ryan <ryan.salles@gmail.com>
2017-08-08 20:25:16 -0300

    Cadastro do funcionário finalizado

---
+++ cadastrar_funcionario.php
@@ -0,0 +1,16 @@
+<?php
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+
+	$con = mysqli_connect("localhost","root","","git2");           
+            
+	$sql = "insert into funcionario values(null,'".$nome."','".$email."')";
+
+	if(mysqli_query($con, $sql)) {
+		echo "Gravado com sucesso!";
+	} else {
+		echo "Erro ao gravar!";
+	}
+
+	mysqli_close($con);
+?>
